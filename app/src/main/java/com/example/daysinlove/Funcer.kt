bc656d7b495c78e4914e0524386ab3730746ccae
Delalex <delalexdev@ya.ru>
2024-08-05 22:01:54 +0300

    init

---
+++ app/src/main/java/com/example/daysinlove/Funcer.kt
@@ -0,0 +1,45 @@
+package com.example.daysinlove
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+import java.util.Date
+
+class Funcer {
+    companion object {
+        fun isBeautifulNumber(n: Int): Boolean {
+            val str = n.toString()
+
+            // Числа из повторяющихся 17 являются красивыми
+            if (str.length % 2 == 0 && str.chunked(2).all { it == "17" }) return true
+
+            // Числа состоящие из одной повторяющейся цифры - красивые
+            if (str.all { it == str[0] }) return true
+
+            // Числа симметричные типа 1881 - красивые
+            if (str == str.reversed()) return true
+
+            // Целые числа являются красивыми
+            if (n % 10 == 0) return true
+
+            // Остальные числа - не красивые
+            return false
+        }
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun getDays(): Int {
+            val data_format = SimpleDateFormat("dd.MM.yyyy")
+            val currentDate = data_format.format(Date())
+            val from = LocalDate.of(2020, 10, 17)
+            val to = LocalDate.now()
+            val daysBetween = ChronoUnit.DAYS.between(from, to)
+            return daysBetween.toInt()
+        }
+        fun getDateString(): String {
+            val data_format = SimpleDateFormat("dd.MM.yyyy")
+            val currentDate = data_format.format(Date())
+            return currentDate.toString()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/daysinlove/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.daysinlove
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // ON READY
+        // list all widgets
+        val labelDatesWidget = findViewById<TextView>(R.id.labelDates)
+        val labelDaysTogetherWidget = findViewById<TextView>(R.id.labelDaysTogether)
+        val calendarImageView = findViewById<ImageView>(R.id.calendar)
+        val share_btn = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.btnShare)
+
+        // date work
+        val daysBetween = Funcer.getDays()
+
+        labelDatesWidget.text = "17.10.2020 - " + Funcer.getDateString()
+        labelDaysTogetherWidget.text = daysBetween.toString()
+
+        // CHECK BEAUTINESS
+        if (Funcer.isBeautifulNumber(daysBetween)) {
+            val drawable = ContextCompat.getDrawable(this, R.drawable.calendar_cool)
+            calendarImageView.setImageDrawable(drawable)
+        }
+
+        // SHARING
+        share_btn.setOnClickListener {
+            val intent = Intent.createChooser(intent, "Поделиться через:")
+
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "\uD83D\uDC99 Мы вместе вот уже " + daysBetween.toString() + " дней \uD83E\uDDE1"
+            )
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Поделиться через:"))
+        }
+    }
+}
